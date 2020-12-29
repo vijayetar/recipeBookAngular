e940918f0a66594b2061646231d7f49a3103befe
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-12-29 13:49:13 -0800

    working on setting up the components of the recipe book

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,6 @@ import {Component} from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  @Input()
+  name:string = 'Vij vij';
 }
